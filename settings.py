cd313d9be3808c56cef76a50f48ea983bc52f967
Geoffrey Kasibante <44870409+kasibanteg@users.noreply.github.com>
2021-12-09 20:13:34 -0500

    Add files via upload
    ddd

---
+++ settings.py
@@ -0,0 +1,9 @@
+#############Change urls in production###############
+#start main app URL
+
+main_app_url = 'http://104.129.128.98:8501/'
+#end main app URL
+
+#start sub_apps URL
+sub_app_url = 'http://104.129.128.98:8502/'
+#end sub_apps URL
